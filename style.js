eff9ec3fe24f8d25dfaf97772d0cdd6602adfb4f
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2023-03-31 18:12:24 +0530

    inital commit

---
+++ style.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const StylePro = StyleSheet.create({
+  k: {
+    Color: 'blue',
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+});
+export {StylePro};
